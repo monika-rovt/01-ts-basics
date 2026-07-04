b96f42ea367d282f5e0ce7fab72d96f8535f6455
Monika Rovt <mirarooti@gmail.com>
2026-07-05 00:27:46 +0300

    8 tasks

---
+++ src/task-5.ts
@@ -0,0 +1,24 @@
+// Функція logStatus приймає рядок status і виводить відповідне повідомлення.
+
+
+
+function logStatus(status: "loading" | "success" | "error"): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
+
+
+
+// Завдання:
+
+// Типізуй параметр status так, щоб дозволити тільки три конкретні рядкові значення: "loading", "success", "error"
+// Переконайся, що TypeScript не дозволяє передати будь-яке інше значення.
+// Типізуй повернення функції, зауваж що вона нічого явно не повертає.
+// Залиш реалізацію функції без змін.
